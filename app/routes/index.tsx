f761d39a6bf3442a681634b91042460530bc8acb
shimahi <shimahi@reah.jp>
2024-03-05 23:12:59 +0900

    fix routing style top

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,24 +1,15 @@
 import Top from '@/components/templates/top'
 import { PostDomain } from '@/domains/post'
-import { createElement } from '@/middlewares'
 import { authMiddlewares } from '@/middlewares/auth'
-import { Hono } from 'hono'
 import { env } from 'hono/adapter'
-import Counter from '@/islands/counter'
-import OwnPostBox from '@/islands/ownPostBox'
+import { createRoute } from 'honox/factory'
 
-const app = new Hono()
-
-app.get('/', authMiddlewares.authorize, async (c) => {
+export default createRoute(authMiddlewares.authorize, async (c) => {
   const currentUser = c.get('currentUser')
 
   const postDomain = new PostDomain(c)
   const posts = await postDomain.pagenatePosts()
-
-  return c.render(
-    createElement(Top, { currentUser, posts, envName: env(c).NAME }),
-    { title: 'Hono Test App' },
-  )
+  return c.render(<Top {...{ currentUser, posts, envName: env(c).NAME }} />, {
+    title: 'Hono Test App',
+  })
 })
-
-export default app
