987e05c709239e01609896bb840ae6eb5fb45c72
shimahi <shimahi@reah.jp>
2024-03-05 23:16:40 +0900

    set counter top

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,6 @@
 import Top from '@/components/templates/top'
 import { PostDomain } from '@/domains/post'
+import Counter from '@/islands/counter'
 import { authMiddlewares } from '@/middlewares/auth'
 import { env } from 'hono/adapter'
 import { createRoute } from 'honox/factory'
@@ -9,7 +10,13 @@ export default createRoute(authMiddlewares.authorize, async (c) => {
 
   const postDomain = new PostDomain(c)
   const posts = await postDomain.pagenatePosts()
-  return c.render(<Top {...{ currentUser, posts, envName: env(c).NAME }} />, {
-    title: 'Hono Test App',
-  })
+  return c.render(
+    <>
+      <Top {...{ currentUser, posts, envName: env(c).NAME }} />
+      <Counter />
+    </>,
+    {
+      title: 'Hono Test App',
+    },
+  )
 })
